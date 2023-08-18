766a410873453c6a76f15e2a5ba0f0295c798d37
vilkazs <vilka_zs@mail.ru>
2023-08-18 16:13:42 +0200

    Added Adder.py

---
+++ Adder.py
@@ -0,0 +1,8 @@
+autotests_completed_yesterday = 321
+autotests_completed_today = '80.2'
+
+# Adder
+autotests_completed_sum = int(autotests_completed_yesterday + float(autotests_completed_today) )
+
+print('Сколько автотестов выполнено?')
+print(f'Выполнено {autotests_completed_sum}')
